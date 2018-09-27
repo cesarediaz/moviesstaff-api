49d0c55ddd4d7a2288baba374164b6e63bd8c48d
César Díaz <cesar.diaz@toptal.com>
2018-09-27 18:45:33 -0300

    added first test for movies#index and api::v1::controller

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -1,16 +1,23 @@
 require 'rails_helper'
 
-describe 'GET /v1/movies' do
-  let!(:movies) {FactoryBot.create_list(:movie, 10) }
+describe 'GET /api/v1/movies' do
+  describe 'when :index' do
+    let!(:movies) {FactoryBot.create_list(:movie, 10) }
 
-  before { get '/v1/movies', headers: { 'Accept': 'application/vnd' } }
+    before { get '/api/v1/movies', headers: { 'Accept': 'application/vnd' } }
 
-  it 'returns HTTP status 200' do
-    expect(response).to have_http_status 200
-  end
+    it 'returns HTTP status 200' do
+      expect(response).to have_http_status 200
+    end
 
-  it 'returns all movies' do
-    body = JSON.parse(response.body)
-    expect(body['data'].size).to eq(10)
+    it 'returns all movies' do
+      json_response = JSON.parse(response.body)
+      expect(json_response.size).to eq(10)
+    end
+
+    it 'should match keys' do
+      json_response = JSON.parse(response.body)
+      expect(json_response.first.keys).to match_array(%w[id title release_year])
+    end
   end
 end
