183e4152e3e056482b32ace7fabf6eb867bf2ad4
César Díaz <cesar.diaz@toptal.com>
2018-09-29 00:26:11 -0300

    added basic actions and views for people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,18 +5,54 @@ class PeopleController < ActionController::Base
     @people = Person.all
   end
 
+  def new
+    @person = Person.new
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @person = Person.new(person_params)
+
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to people_path, notice: 'Person was successfully created.' }
+      else
+        format.html { render action: "new" }
+      end
+    end
+  end
+
   def edit
+    @person = Person.find(params[:id])
   end
 
   def update
+    @person = Person.find(params[:id])
+
+    respond_to do |format|
+      if @person.update_attributes(person_params)
+        format.html { redirect_to people_path, notice: 'Ad was successfully updated.' }
+      else
+        format.html { render action: "edit" }
+      end
+    end
   end
 
   def destroy
+    @person = Person.find(params[:id])
+    @person.destroy
+
+    respond_to do |format|
+      format.html { redirect_to people_url }
+    end
   end
-  
+
   private
 
-  def people_params
-    params.require(:people).permit(:id, :first_name, :last_name)
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :aliases)
   end
 end
